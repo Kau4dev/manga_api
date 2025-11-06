68a30069244bd52adad26daadb172211417cf95b
kau4dev <kauavictordev7@gmail.com>
2025-11-06 11:02:38 -0300

    feat: adiciona tela de navegação principal com BottomNavigationBar

---
+++ lib/screens/main_navigation_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+//import 'home_screen.dart';
+//import 'search_screen.dart';
+//import 'favorites_screen.dart';
+
+class MainNavigationScreen extends StatefulWidget {
+  const MainNavigationScreen({super.key});
+
+  @override
+  State<MainNavigationScreen> createState() => _MainNavigationScreenState();
+}
+
+class _MainNavigationScreenState extends State<MainNavigationScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = const [
+    // HomeScreen(),
+    //SearchScreen(),
+    //FavoritesScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(index: _currentIndex, children: _screens),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Início'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Buscar'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favoritos',
+          ),
+        ],
+      ),
+    );
+  }
+}
